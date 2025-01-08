7b007d4b1bcb312b5f0b565b80c37387c8b2afa0
Paulogit24 <40240257@esmad.ipp.pt>
2025-01-08 15:11:35 +0000

    inicio

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 import customtkinter
+import os
+import datetime
+import CTkMessagebox
+from  tkinter import ttk
+from  PIL import Image, ImageTk
+
+fUsers = ".\\users.txt"
+
+def logacount():
+    print
